3d18a5d21edc7d26f8ffe8865f83b2db387f39f1
saurabhkawatra <saurabh.kawatra1@gmail.com>
2021-12-01 23:35:46 +0530

    Project Created Using Eclipse IDE and added the required dependencies

---
+++ YebeloAssignmentCodeBase/src/main/java/com/yebelo/rest/YebeloAssignmentCodeBaseApplication.java
@@ -0,0 +1,13 @@
+package com.yebelo.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YebeloAssignmentCodeBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YebeloAssignmentCodeBaseApplication.class, args);
+	}
+
+}
